f93e6f100565cc4efa1f7b7b11cb1645caa39438
USHADHARSHINI VS <134365652+USHADHARSHINI97@users.noreply.github.com>
2024-05-08 21:51:54 +0530

    Add files via upload

---
+++ relative_ranks.go
@@ -0,0 +1,62 @@
+/*
+You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+The 1st place athlete's rank is "Gold Medal".
+The 2nd place athlete's rank is "Silver Medal".
+The 3rd place athlete's rank is "Bronze Medal".
+For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+Example 1:
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+
+Example 2:
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func findRelativeRanks(score []int) []string {
+	n := len(score)
+	// Create a copy of the score slice to sort
+	sortedScore := make([]int, n)
+	copy(sortedScore, score)
+	sort.Sort(sort.Reverse(sort.IntSlice(sortedScore)))
+	rankMap := make(map[int]string)
+	for i, s := range sortedScore {
+		switch i {
+		case 0:
+			rankMap[s] = "Gold Medal"
+		case 1:
+			rankMap[s] = "Silver Medal"
+		case 2:
+			rankMap[s] = "Bronze Medal"
+		default:
+			rankMap[s] = fmt.Sprintf("%d", i+1)
+		}
+	}
+	result := make([]string, n)
+	for i, s := range score {
+		result[i] = rankMap[s]
+	}
+
+	return result
+}
+func main() {
+	score1 := []int{5, 4, 3, 2, 1}
+	score2 := []int{10, 3, 8, 9, 4}
+
+	fmt.Println(findRelativeRanks(score1)) // Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+	fmt.Println(findRelativeRanks(score2)) // Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+}
